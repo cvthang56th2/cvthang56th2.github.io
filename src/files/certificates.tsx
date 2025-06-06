2308e61cca6057738eb2f3e0430288ae623144db
Thang Cao <cvthang56th2@gmail.com>
2025-06-06 19:06:37 +0700

    update cert

--- src/files/certificates.tsx
+++ src/files/certificates.tsx
@@ -1,4 +1,5 @@
 import vueCertificate from '../vue-certificate.png';
+import awsSAACertificate from '../aws-saa.png';
 
 const Certificates = () => {
   return (
@@ -9,8 +10,12 @@ const Certificates = () => {
           <li><a className="text-blue-500 hover:text-purple-500" href="https://www.credly.com/badges/39560fc2-e960-4a55-8c91-271ff1b987e7" target="_blank" rel="noopener noreferrer">Microsoft Ofice Specialist: Word 2013 (2018)</a></li>
           <li><a className="text-blue-500 hover:text-purple-500" href="https://www.credly.com/badges/c2b6fddd-6fc4-4202-9e30-7160ee7990ad" target="_blank" rel="noopener noreferrer">Microsoft Ofice Specialist: Powerpoint 2013 (2018)</a></li>
           <li><a className="text-blue-500 hover:text-purple-500" href="https://badgr.com/public/assertions/jcKxBDueQuurY5VktOIhvg" target="_blank" rel="noopener noreferrer">Certified Vue Developer (by VueAcademy – 2023)</a></li>
-          <a className="text-blue-500 hover:text-purple-500" href="https://badgr.com/public/assertions/jcKxBDueQuurY5VktOIhvg" target="_blank" rel="noopener noreferrer">
-            <img src={vueCertificate} alt="thang-cao-vue-certificate" className='w-full md:w-1/2 mx-auto mt-10' />
+          <a className="text-blue-500 hover:text-purple-500 block my-4" href="https://badgr.com/public/assertions/jcKxBDueQuurY5VktOIhvg" target="_blank" rel="noopener noreferrer">
+            <img src={vueCertificate} alt="thang-cao-vue-certificate" className='w-full md:w-1/2 mx-auto' />
+          </a>
+          <li><a className="text-blue-500 hover:text-purple-500" href="https://www.credly.com/badges/9597eb9e-fe0a-48fb-9a31-b063130a0328/linked_in?t=svyryr" target="_blank" rel="noopener noreferrer">AWS Certified Solutions Architect – Associate (2025)</a></li>
+          <a className="text-blue-500 hover:text-purple-500 block my-4" href="https://www.credly.com/badges/9597eb9e-fe0a-48fb-9a31-b063130a0328/linked_in?t=svyryr" target="_blank" rel="noopener noreferrer">
+            <img src={awsSAACertificate} alt="thang-cao-AWS-SAA-certificate" className='w-full md:w-1/4 mx-auto' />
           </a>
         </ul>
       </div>
